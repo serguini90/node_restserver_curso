2d68fb274819354d32367b7be7dd967d519d0d88
serguini90@gmail.com <smms9030>
2020-04-13 10:05:54 -0500

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,47 @@
+require('./config/config');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+app.get('/usuario', (req, res) => {
+    res.json('Get usuarios');
+});
+
+app.post('/usuario', (req, res) => {
+    let body = req.body;
+    if(body.nombre === undefined ){
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    } else {
+        res.json(body);
+    }
+});
+
+app.put('/usuario/:id', (req, res) => {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json('Delete usuarios');
+});
+
+
+
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Escuchando puerto: ${process.env.PORT}`)
+});
